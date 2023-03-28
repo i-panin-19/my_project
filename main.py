66f572da6975cd50890277c3a51e1714bc426a42
i-panin-19 <ilypanin@gmail.com>
2023-03-29 00:08:39 +0700

    editing a file main.py applying the subtraction function

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ import funcs
 numA = int(input('Введите число A: '))
 numB = int(input('Введите число B: '))
 print(f'Сумма чисел: {funcs.sum_two_num(numA, numB)}')
+print(f'Вычитание чисел: {funcs.subtr_two_num(numA, numB)}')
